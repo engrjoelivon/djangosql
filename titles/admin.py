87ecd604a38f441721985bc594edc6fdc1fd57f6
joel ivongbe <engrjoelivon@yahoo.com>
2015-10-20 01:27:42 -0500

    added admin

--- titles/admin.py
+++ titles/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from titles.models import Numeric
+from titles.models import Numeric,Sample2
 
 # Register your models here.
 admin.site.register(Numeric)
+admin.site.register(Sample2)
